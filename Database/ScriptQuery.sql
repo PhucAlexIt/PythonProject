7a185e46731efaa50484d3935c10959aca6b8735
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2025-09-30 23:10:19 +0700

    upload file

---
+++ Database/ScriptQuery.sql
@@ -0,0 +1,4 @@
+create database Query_Script
+use bike_store
+
+select * from sales.customers
--- PyProject.py
+++ PyProject.py
@@ -2,3 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+df = pd.read_csv('File_Analyst/chipotle.tsv', sep='\t')
+df.item_price = df.item_price.apply(lambda x : float(x.replace('$','')))
+df["total_price"] = df["quantity"] * df["item_price"]
+# total = df["total_price"].sum()
+# print(f' Total price: {total}')
+# print(df.groupby("item_name").apply(print))
+# c = df.groupby("item_name")["total_price"].sum()
+# print(df.item_name.value_counts())
+# print(df.item_name.value_counts().count())
+print(df.item_name.nunique())
+# print(c.sort_values(ascending=False).head(10))
+# print(df.head(6))
